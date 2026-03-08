188a011f043676aeb5af28ae297482d6dc865b75
Sivaram Yadav Nalliboyana <nsivaramdav@gmail.com>
2026-03-08 22:24:59 +0100

    Updated main agent to include analyzt

--- main_agent.py
+++ main_agent.py
@@ -1,7 +1,7 @@
 import sqlite3
 from typing import TypedDict, Optional
 from langgraph.graph import StateGraph, END
-from code_node import generate_sql
+from code_node import generate_sql, llm
 from executor_node import execute_sql
 
 # Define states
@@ -11,6 +11,7 @@ class AgentState(TypedDict):
     db_result: Optional[list]
     error: Optional[str]
     attempts: int
+    final_answer: str
 
 def sql_generator_node(state: AgentState):
     query, schema = generate_sql(state['question'])
@@ -24,24 +25,43 @@ def sql_executor_node(state: AgentState):
         return {"error": result["message"]}
 
 def should_continue(state: AgentState):
-    if state["error"] is None or state["attempts"] >= 3:
-        return "end"
+    if state["error"] is None:
+        return "summarize"  # Go to Analyst
+    elif state["attempts"] >= 3:
+        return "end"  # Give up
     else:
-        return "retry"
+        return "retry"  # Back to Coder
+
+
+def analyst_node(state: AgentState):
+    # We ask Llama to explain the data
+    prompt = f"""
+    User Question: {state['question']}
+    Database Result: {state['db_result']}
+
+    Based on the data above, write a natural-sounding answer to the user. 
+    Be concise. Don't mention 'SQL' or 'Database'.
+    """
+
+    answer = llm.invoke(prompt)
+    return {"final_answer": answer.strip()}
 
 # Build langgraph
 workflow = StateGraph(AgentState)
 workflow.add_node("coder", sql_generator_node)
 workflow.add_node("executor", sql_executor_node)
+workflow.add_node("analyst", analyst_node)
 
 workflow.set_entry_point("coder")
 workflow.add_edge("coder", "executor")
+workflow.add_edge("analyst", END)
 
 workflow.add_conditional_edges(
     "executor",
     should_continue,
     {
         "retry": "coder",
+        "summarize": "analyst",
         "end": END
     }
 )
@@ -49,10 +69,16 @@ workflow.add_conditional_edges(
 app = workflow.compile()
 
 if __name__ == "__main__":
-    inputs = {"question": "What is the price of the Laptop?", "attempts": 0}
+    inputs = {"question": "What is the most expensive item in the shop?", "attempts": 0}
+
+    # 2. Start the Graph and wait for it to finish
     final_state = app.invoke(inputs)
 
-    print("\n--- FINAL RESULT ---")
-    print(f"Question: {final_state['question']}")
-    print(f"SQL Used: {final_state['sql_query']}")
-    print(f"Data Found: {final_state['db_result']}")
+
+    # 3. This is the part the 'Analyst' wrote!
+    print(f"ANSWER: {final_state.get('final_answer')}")
+
+    print("\n--- Technical Audit ---")
+    print(f"SQL used: {final_state['sql_query']}")
+    print(f"Raw Data: {final_state['db_result']}")
+    print("=" * 30)
